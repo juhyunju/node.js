6a9296cc2d0362dccb0bb3376881f5eb76c3a79b
juhyunju <2juhyunju@naver.com>
2022-01-03 13:52:38 +0900

    GET 매소드 추가

---
+++ REST/restServer.js
@@ -0,0 +1,41 @@
+const http = require('http')
+const fs = require('fs').promises
+
+const users = {}
+
+const server = http.createServer(async (req,res) => {
+  try{
+    console.log(req.method, req.url)
+    if(req.method === 'GET') {
+      if(req.url === '/') {
+        const data = await fs.readFile('./restFront.html')
+        res.writeHead(200,{ 'Content-Type' : 'text/html; charset=utf-8'})
+        return res.end(data)
+      } else if (req.url === '/about'){
+        const data = await fs.readFile('./about.html')
+        res.writeHead(200,{ 'Content-Type' : 'text/html; charset=utf-8'})
+        return res.end(data)
+      }else if (req.url === '/users'){
+        res.writeHead(200,{ 'Content-Type' : 'text/plain; charset=utf-8'})
+        return res.end(JSON.stringify(users))
+      }
+      // 주소가 /도 /about도 아니면
+      try{
+        const data = await fs.readFile(`.${req.url}`)
+        return res.end(data)
+      }catch(err){
+        //주소가 해당하는 라우트를 못 찾았다는 404 Not Found error 발생
+      }
+    }
+    res.writeHead(404)
+    return res.end('NOT FOUND')
+  }catch(err){
+    console.error(err)
+    res.writeHead(200,{ 'Content-Type' : 'text/plain; charset=utf-8'})
+    res.end(err.message)
+  }
+})
+server.listen(8080)
+server.on('listening', () => {
+  console.log('8080번 포트에서 서버 대기 중입니다.')
+})
